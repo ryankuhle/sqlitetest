f2b67d9658a7a981586701eb2493bf1989fb93da
Ryan Kuhle <ryan@ryankuhle.com>
2015-10-26 20:11:33 -0500

    Create project outline

---
+++ database.py
@@ -0,0 +1,16 @@
+#Goal: print out the hottest cities in July with following format:
+#The cities that are warmest in July are: city, state, city, state, etc.
+
+#Connect to getting_started.db
+
+#Drop tables "cities" and "weather" if they exist
+
+#Create "cities" and "weather" tables
+
+#Insert the appropriate data into tables
+
+#Join the data together
+
+#Load into a pandas DataFrame
+
+#Print out the resulting city and state in a full sentence. For example: "The cities that are warmest in July are: Las Vegas, NV, Atlanta, GA..."
